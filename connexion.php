1d5709f5ad85e38ef779b64938e093c89a0588c3
Morgan Lechevallier <morgan@example.com>
2026-05-20 09:40:29 +0200

    essai 2

--- connexion.php
+++ connexion.php
@@ -5,7 +5,7 @@ session_start()
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style.css?v=11">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sip & Spill - Connexion</title>
